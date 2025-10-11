e42e20896699bde466ec234f0d1d4809e40146f2
Maksym Sergiyenko <maksymsergiyenko09@gmail.com>
2025-10-11 16:18:25 +0300

    update script

--- script.sh
+++ script.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-echo "Підрахунок файлів у /etc..."
+
+echo "Counting files in /etc..."
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: This script must be run as root or with sudo privileges" >&2
+    exit 1
+fi
+
+if [ ! -d "/etc" ]; then
+    echo "Error: Directory /etc does not exist!" >&2
+    exit 1
+fi
 
 count=$(find /etc -type f 2>/dev/null | wc -l)
 
-echo "Кількість файлів: $count"
+if [ $? -ne 0 ]; then
+    echo "Error: find command failed" >&2
+    exit 1
+fi
+
+echo "Number of regular files: $count"
+exit 0
